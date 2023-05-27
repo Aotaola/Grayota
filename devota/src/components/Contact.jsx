84973cc11ef04e88b55a30513e2bb21252a55752
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-05-26 23:36:52 -0400

    cleanup

--- devota/src/components/Contact.jsx
+++ devota/src/components/Contact.jsx
@@ -21,13 +21,13 @@ const Contact = () => {
     };
 
     return ( 
-        <>
-        <div className="contact-container">
+        <div className="Contact-page">
+            <div className="contact-container">
+               <h1>Lets Connect!</h1>
 
-        <br/>
-            <form className="ContactForm" onSubmit={handleSubmit}>
-               <h1>Contact me!</h1>
-                <input 
+                 <br/>
+                {/* <form className="ContactForm" onSubmit={handleSubmit}> */}
+                {/* <input 
                     name="name" 
                     type="text" 
                     placeholder="Your name" 
@@ -48,9 +48,46 @@ const Contact = () => {
                     onChange={handleChange}
                     />
                 <button type="submit">Submit</button>
-            </form>
-                    </div>
-        </>
+                 </form> */}
+                    <div className="link-block">
+                        <div className="email-block">
+
+                         <h3> preffered email</h3> 
+
+                            <a href = 'Alejandro@grayota.com'>
+
+                                <img src="https://www.freepnglogos.com/uploads/logo-outlook-png/file-microsoft-office-outlook-logo-present-svg-wikipedia-1.png" width="200" alt="hd microsoft outlook logo brand icon png" className="link-img"/>
+
+                            </a>
+                        </div>
+                            <br />
+                         <div className="linkedin-block">
+                                
+
+                            <h3> My LinkedIn </h3> 
+
+                                <a href = 'https://www.linkedin.com/in/aotaola'>
+
+                                    <img src="https://www.freepnglogos.com/uploads/linkedin-logo-design-30.png" width="200" alt="linkedin logo design" className="link-img"/>
+
+                                </a>
+                        </div>
+                            <br/>
+                        <div className="github-block">
+
+                            <h3> My Github </h3> 
+
+                                <a href = 'https://github.com/Aotaola'>
+
+                                    <img src="https://www.freepnglogos.com/uploads/512x512-logo/512x512-transparent-logo-github-logo-24.png" width="200" alt="512x512 transparent logo, github logo" className="link-img" />
+
+                                </a>
+
+                        </div>
+
+                     </div>
+            </div>
+        </div>
      );
 }
  
